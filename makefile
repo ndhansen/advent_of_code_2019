cf3d1d26a9d5a09e5a7a8ce43ac9566b518c247f
Nicholas Hansen <ndhansen@leskat.net>
2024-11-03 21:13:44 +0000

    Completed day 5

--- makefile
+++ makefile
@@ -2,15 +2,15 @@
 
 .venv/.venvsetup:
 	python3.13 -m venv .venv
-	.venv/bin/python3 -m pip install --upgrade pip
-	.venv/bin/python3 -m pip install hatch uv
-	touch @a
+	.venv/bin/python3 -m pip install --upgrade pip uv
+	.venv/bin/python3 -m uv pip install hatch
+	touch $@
 
 
 .venv/.devsetup: .venv/.venvsetup
 	.venv/bin/python3 -m uv pip install -e .[dev]
 	.venv/bin/python3 -m uv pip install -e ../advent_of_code_utils
-	touch @a
+	touch $@
 
 develop: .venv/.devsetup
 
--- src/aoc_2019/day_5.py
+++ src/aoc_2019/day_5.py
@@ -4,8 +4,6 @@ import enum
 from dataclasses import dataclass
 from typing import Any
 
-import pudb
-
 from aoc_2019.day_2 import PuzzleInput
 
 
@@ -13,6 +11,10 @@ class OpCode(enum.Enum):
     ADD = 1
     MULTIPLY = 2
     OUTPUT = 4
+    JIT = 5
+    JIF = 6
+    LT = 7
+    EQ = 8
 
 
 class Mode(enum.Enum):
@@ -30,57 +32,96 @@ class Parameter:
         padded = str(number).rjust(5, "0")
         opcode = OpCode(int(padded[-2:]))
 
-        modes = [Mode(int(x)) for x in padded[-3::-1]]
-        in_order_modes = list(reversed(modes))
+        match opcode:
+            case OpCode.OUTPUT:
+                modes = [Mode(int(padded[2]))]
+            case OpCode.JIT | OpCode.JIF:
+                modes = [Mode(int(x)) for x in padded[-3:0:-1]]
+            case OpCode.ADD | OpCode.MULTIPLY | OpCode.LT | OpCode.EQ:
+                modes = [Mode(int(x)) for x in padded[-3::-1]]
         return Parameter(
             opcode=opcode,
-            modes=in_order_modes,
+            modes=modes,
         )
 
-    def show(self, numbers: list[int]) -> None:
+    def eval(self, idx: int, numbers: list[int]) -> int:
+        # print(self.debug_output(idx, numbers))
         match self.opcode:
-            ...
+            case OpCode.ADD:
+                first, second = self.get_inputs(idx, numbers)
+                output = first + second
+                numbers[numbers[idx + 3]] = output
+                return idx + 4
+            case OpCode.MULTIPLY:
+                first, second = self.get_inputs(idx, numbers)
+                output = first * second
+                numbers[numbers[idx + 3]] = output
+                return idx + 4
+            case OpCode.OUTPUT:
+                match self.modes[0]:
+                    case Mode.IMMEDIATE:
+                        output = numbers[idx + 1]
+                    case Mode.POSITION:
+                        output = numbers[numbers[idx + 1]]
+                print("Output:", output)
+                return idx + 2
+            case OpCode.JIT:
+                to_check, out = self.get_inputs(idx, numbers)
+                match to_check:
+                    case 0:
+                        return idx + 3
+                    case _:
+                        return out
+            case OpCode.JIF:
+                to_check, out = self.get_inputs(idx, numbers)
+                match to_check:
+                    case 0:
+                        return out
+                    case _:
+                        return idx + 3
+            case OpCode.LT:
+                first, second = self.get_inputs(idx, numbers)
+                if first < second:
+                    numbers[numbers[idx + 3]] = 1
+                else:
+                    numbers[numbers[idx + 3]] = 0
+                return idx + 4
+            case OpCode.EQ:
+                first, second = self.get_inputs(idx, numbers)
+                if first == second:
+                    numbers[numbers[idx + 3]] = 1
+                else:
+                    numbers[numbers[idx + 3]] = 0
+                return idx + 4
 
-def get_inputs(parameter: Parameter, idx: int, numbers: list[int]) -> tuple[int, int]:
-    match parameter.modes[0]:
-        case Mode.POSITION:
-            first = numbers[numbers[idx + 1]]
-        case Mode.IMMEDIATE:
-            first = numbers[idx + 1]
-    match parameter.modes[1]:
-        case Mode.POSITION:
-            second = numbers[numbers[idx + 2]]
-        case Mode.IMMEDIATE:
-            second = numbers[idx + 2]
+    def get_inputs(self, idx: int, numbers: list[int]) -> tuple[int, int]:
+        match self.modes[0]:
+            case Mode.POSITION:
+                first = numbers[numbers[idx + 1]]
+            case Mode.IMMEDIATE:
+                first = numbers[idx + 1]
+        match self.modes[1]:
+            case Mode.POSITION:
+                second = numbers[numbers[idx + 2]]
+            case Mode.IMMEDIATE:
+                second = numbers[idx + 2]
 
-    return first, second
+        return first, second
 
-
-def eval_parameter(parameter: Parameter, idx: int, numbers: list[int]) -> int:
-    match parameter.opcode:
-        case OpCode.ADD:
-            first, second = get_inputs(parameter, idx, numbers)
-            numbers[numbers[idx + 3]] = first + second
-            return 4
-        case OpCode.MULTIPLY:
-            first, second = get_inputs(parameter, idx, numbers)
-            numbers[numbers[idx + 3]] = first * second
-            return 4
-        case OpCode.OUTPUT:
-            match parameter.modes[2]:
-                case Mode.IMMEDIATE:
-                    output = numbers[idx + 1]
+    def debug_output(self, idx: int, numbers: list[int]) -> str:
+        text = "OpCode: " + self.opcode.name
+        text += "\n  Modes:"
+        for i, mode in enumerate(self.modes, start=1):
+            text += f"\n    mode {i}: {mode.name}: "
+            match mode:
                 case Mode.POSITION:
-                    output = numbers[numbers[idx + 1]]
-            if output != 0:
-                import pudb
-
-                pudb.set_trace()
-            print(output)
-            return 2
+                    text += f"({numbers[idx+i]}) {numbers[numbers[idx+i]]}"
+                case Mode.IMMEDIATE:
+                    text += f"{numbers[idx+i]}"
+        return text
 
 
-def run_program(program: list[int]) -> Any:
+def run_program(program: list[int], user_input: int) -> Any:
     numbers = program.copy()
 
     idx = 0
@@ -90,21 +131,21 @@ def run_program(program: list[int]) -> Any:
             case 99:
                 break
             case 3:
-                numbers[numbers[idx + 1]] = 1
+                numbers[numbers[idx + 1]] = user_input
                 idx += 2
             case _:
-                print("debug:", numbers[idx])
                 parameter = Parameter.parse(numbers[idx])
-                moves = eval_parameter(parameter, idx, numbers)
-                idx += moves
+                idx = parameter.eval(idx, numbers)
 
 
 def part_1(puzzle: PuzzleInput) -> Any:
-    numbers = [int(x) for x in puzzle.raw.split(",")]
+    numbers = [int(x) for x in puzzle.raw.replace("\n", "").split(",")]
 
-    pudb.set_trace()
-
-    run_program(numbers)
+    run_program(numbers, 1)
 
 
-def part_2(puzzle: PuzzleInput) -> Any: ...
+def part_2(puzzle: PuzzleInput) -> Any:
+    numbers = [int(x) for x in puzzle.raw.replace("\n", "").split(",")]
+
+    print("Part 2:")
+    run_program(numbers, 5)
